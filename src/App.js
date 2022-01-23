e1acf4c4e4c65ecf4c2a42fcc203d717b3f62aec
Joy Rhee <joyfullyx@gmail.com>
2022-01-22 17:02:18 -0800

    edit comments

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
     allEmployees: [],
   });
 
+  // fetch data
   const getEmployees = () => {
     API.employees()
       .then((res) => {
@@ -49,29 +50,3 @@ const styles = {
     paddingBottom: "20px",
   },
 };
-
-// Product Requirements
-// -----------------------------
-
-// - Use React to build a page that lists employees.
-// - comp.png is provided for visual reference (UX info below)
-// - index.js is your starting point.
-// - employees.json includes the data, and is already imported into index.js
-// - Build components with reusability in mind.
-
-// UX Requirements
-// ------------------------------
-// - background color: #f2f3f5
-// - card color: #ffffff
-// - text color: #60646d
-// - quote banner color: #dce0e8
-// - profile pics are 150px circle
-// - profile pics have 5px border of employee's favorite color
-// - "Contact Me" button will initiate an email to employee's email address when clicked
-// - "Contact Me" button is the employee's favorite color
-// - (extra credit - styling) the "Contact Me" button color lightens 5% on hover
-// - (extra credit - styling) the quote banner extends beyond the card horizontally
-
-// Extra
-// -------------------------------
-// - you can fetch data from the following link instead of using data https://run.mocky.io/v3/ce160a84-83a8-45cf-9cf7-755d61acef54
